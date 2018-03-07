6f73944f1b9594bab09db52f53e67191ef709cd4
DmitriyFokin <fokinds1@gmail.com>
2018-03-07 10:36:29 +0300

    finished 3 of 9

---
+++ PaintORama/PaintORamaDlg.cpp
@@ -0,0 +1,214 @@
+
+// PaintORamaDlg.cpp : файл реализации
+//
+
+#include "stdafx.h"
+#include "PaintORama.h"
+#include "PaintORamaDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Диалоговое окно CAboutDlg используется для описания сведений о приложении
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// Данные диалогового окна
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // поддержка DDX/DDV
+
+// Реализация
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// диалоговое окно CPaintORamaDlg
+
+
+
+CPaintORamaDlg::CPaintORamaDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(IDD_PAINTORAMA_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CPaintORamaDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CPaintORamaDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_CLEARBTN, &CPaintORamaDlg::OnClickedClearbtn)
+	ON_WM_LBUTTONDOWN()
+	ON_WM_MOUSEMOVE()
+	ON_WM_LBUTTONUP()
+END_MESSAGE_MAP()
+
+
+// обработчики сообщений CPaintORamaDlg
+
+BOOL CPaintORamaDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Добавление пункта "О программе..." в системное меню.
+
+	// IDM_ABOUTBOX должен быть в пределах системной команды.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// Задает значок для этого диалогового окна.  Среда делает это автоматически,
+	//  если главное окно приложения не является диалоговым
+	SetIcon(m_hIcon, TRUE);			// Крупный значок
+	SetIcon(m_hIcon, FALSE);		// Мелкий значок
+
+	// TODO: добавьте дополнительную инициализацию
+
+	CWnd* p_Canvas = GetDlgItem(IDC_CANVAS);
+	p_Canvas->GetWindowRect(&m_Canvas);
+	ScreenToClient(&m_Canvas);
+	m_Canvas.DeflateRect(2, 2, 1, 1);
+
+	return TRUE;  // возврат значения TRUE, если фокус не передан элементу управления
+}
+
+void CPaintORamaDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// При добавлении кнопки свертывания в диалоговое окно нужно воспользоваться приведенным ниже кодом,
+//  чтобы нарисовать значок.  Для приложений MFC, использующих модель документов или представлений,
+//  это автоматически выполняется рабочей областью.
+
+void CPaintORamaDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // контекст устройства для рисования
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Выравнивание значка по центру клиентского прямоугольника
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Нарисуйте значок
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// Система вызывает эту функцию для получения отображения курсора при перемещении
+//  свернутого окна.
+HCURSOR CPaintORamaDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CPaintORamaDlg::OnClickedClearbtn()
+{
+	// TODO: добавьте свой код обработчика уведомлений
+
+	CClientDC dc(this);
+
+	dc.SelectStockObject(NULL_PEN);
+
+	dc.Rectangle(m_Canvas);
+}
+
+
+void CPaintORamaDlg::OnLButtonDown(UINT nFlags, CPoint point)
+{
+	// TODO: добавьте свой код обработчика сообщений или вызов стандартного
+	if (m_Canvas.PtInRect(point))
+	{
+		m_LineStart = point;
+		m_LineEnd = point;
+		SetCapture();
+	}
+
+	CDialogEx::OnLButtonDown(nFlags, point);
+}
+
+
+void CPaintORamaDlg::OnMouseMove(UINT nFlags, CPoint point)
+{
+	// TODO: добавьте свой код обработчика сообщений или вызов стандартного
+	if ((nFlags & MK_LBUTTON) && m_Canvas.PtInRect(point))
+	{
+	CClientDC dc(this);
+	m_LineEnd = point;
+	dc.MoveTo(m_LineStart);
+	dc.LineTo(m_LineEnd);
+	m_LineStart = m_LineEnd;
+	}
+
+	CDialogEx::OnMouseMove(nFlags, point);
+}
+
+
+void CPaintORamaDlg::OnLButtonUp(UINT nFlags, CPoint point)
+{
+	// TODO: добавьте свой код обработчика сообщений или вызов стандартного
+	ReleaseCapture();
+
+	CDialogEx::OnLButtonUp(nFlags, point);
+}
